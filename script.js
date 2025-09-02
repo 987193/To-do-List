13ab4e0d281dc31749d1b287410f2b1af5ad563c
987193 <jindalk198@gmail.com>
2025-09-03 03:30:07 +0530

    added some js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const inputBox=document.getElementById('inputBox');
 const addBtn=document.getElementById('addBtn');
 const todoList=document.getElementById('todoList');
+let editTodo = null;
 const addTodo = ()=> {
     const inputText=inputBox.value.trim();
     if(inputText.length<=0){
         alert("Yor must write something in your to do");
+        return false;
    }
-   else {
+//    if(addBtn.value==="Edit"){
+//     editTodo.target.pre
+//    }
     // Creating p tag 
     const li=document.createElement('li');
     const p=document.createElement('p');
@@ -15,17 +19,33 @@ const addTodo = ()=> {
     todoList.appendChild(li);
      inputBox.value="";
 
+     // creating edit button
+    const EditBtn= document.createElement('button');
+    EditBtn.innerText='Edit';
+    EditBtn.classList.add("btn","EditBtn");
+    li.appendChild(EditBtn);
+
      // creating remove button
     const DeleteBtn= document.createElement('button');
     DeleteBtn.innerText='Remove';
-    DeleteBtn.classList.add("btn");
+    DeleteBtn.classList.add("btn","DeleteBtn");
     li.appendChild(DeleteBtn);
-    // creating edit button
-    const EditBtn= document.createElement('button');
-    EditBtn.innerText='Edit';
-    EditBtn.classList.add("btn");
-    li.appendChild(EditBtn);
+    
    }
 }
-addBtn.addEventListener('click',addTodo);
+const updateTodo=(e)=>{
+    // console.log(e.target.innerHTML);
+    if(e.target.innerHTML=== "Remove"){
+        todoList.removeChild(e.target.parentElement);
+    }
+    if(e.target.innerHTML=== "Edit"){
+        inputBox.value=e.target.previousElementSibling.innerHTML;
+        inputBox.focus();
+        addBtn.value="Edit";
+        editTodo=e;
 
+    }
+    
+}
+addBtn.addEventListener('click',addTodo);
+todoList.addEventListener('click',updateTodo);
